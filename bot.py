62d412161f03a7d1cac8f95625c58da9e7dc844a
Yoga Pranata <zyxdevs@otakufanshare.eu.org>
2022-04-23 06:25:01 +0700

    added more services

--- bot.py
+++ bot.py
@@ -13,6 +13,8 @@ SHRINKADS_KEY = environ.get("SHRINKADS_KEY", None)
 ZAGL_KEY = environ.get("ZAGL_KEY", None)
 YOUSHORT_KEY = environ.get("YOUSHORT_KEY", None)
 PFL_KEY = environ.get("PFL_KEY", None)
+SEARN_KEY = environ.get("SEARN_KEY", None)
+CFLY_KEY = environ.get("CFLY_KEY", None)
 
 
 bot = Client(
@@ -30,15 +32,15 @@ async def start(_, message):
 I'm **MultiSafelinkBot**, Just send me a link with some command below.
 
 **Here is my command:**
-/start - Start me
-/help - See my help
+/adtival <link> - Get a shortlink using adtival
+/cfly <link> - Get a shortlink using clicksfly
 /dlink <link> - Get a shortlink using droplink
-/adtival <link> - Get a shortlink using adtival/wtspw
-/yshort <link> - Get a shortlink using yourshort
-/snack <link> - Get a shortlink using snacklink
 /sads <link> - Get a shortlink using shrinkads
-/zagl <link> - Get a shortlink using zagl
+/sarn <link> - Get a shortlink using shrinkearn
+/snack <link> - Get a shortlink using snacklink
 /pfl <link> - Get a shortlink using paid4link
+/yshort <link> - Get a shortlink using yourshort
+/zagl <link> - Get a shortlink using zagl
 
 More features will added soon:)
 
@@ -219,6 +221,58 @@ async def pfl_handler(_, message):
     link = link.replace(" ", "")
     try:
         r = get(f"https://paid4link.com/api?api={PFL_KEY}&url={link}").json()
+        short_link = r["shortenedUrl"]
+        return await message.reply(
+            f"""Click to copy:\n\n<code>{short_link}</code>.\n\nHere is your [short link]({short_link}).""",
+            quote=True,
+            disable_web_page_preview=True,
+        )
+    except Exception as e:
+        return await message.reply(f"Error: {e}\n\nReport to @Yoga_CIC", quote=True)
+
+
+@bot.on_message(filters.command("sarn") & ~filters.edited)
+async def searn_handler(_, message):
+    if len(message.command) < 2:
+        return await message.reply("Give me url to short!")
+    if message.from_user.id not in AUTH_USERS:
+        return await message.reply(
+            "You are not allowed to use me!\n\nContact @Yoga_CIC"
+        )
+    if SEARN_KEY is None:
+        return await message.reply(
+            "Get `SEARN_KEY` from [shrinkearn](https://shrinkearn.com/ref/YogaPranata) and fill it on vars!", disable_web_page_preview=True
+        )
+    link = message.text.split(None, 1)[1].strip()
+    link = link.replace(" ", "")
+    try:
+        r = get(f"https://shrinkearn.com/api?api={SEARN_KEY}&url={link}").json()
+        short_link = r["shortenedUrl"]
+        return await message.reply(
+            f"""Click to copy:\n\n<code>{short_link}</code>.\n\nHere is your [short link]({short_link}).""",
+            quote=True,
+            disable_web_page_preview=True,
+        )
+    except Exception as e:
+        return await message.reply(f"Error: {e}\n\nReport to @Yoga_CIC", quote=True)
+
+
+@bot.on_message(filters.command("cfly") & ~filters.edited)
+async def cfly_handler(_, message):
+    if len(message.command) < 2:
+        return await message.reply("Give me url to short!")
+    if message.from_user.id not in AUTH_USERS:
+        return await message.reply(
+            "You are not allowed to use me!\n\nContact @Yoga_CIC"
+        )
+    if CFLY_KEY is None:
+        return await message.reply(
+            "Get `CFLY_KEY` from [clicksfly](https://clicksfly.com/ref/YogaPranata) and fill it on vars!", disable_web_page_preview=True
+        )
+    link = message.text.split(None, 1)[1].strip()
+    link = link.replace(" ", "")
+    try:
+        r = get(f"https://clicksfly.com/api?api={CFLY_KEY}&url={link}").json()
         short_link = r["shortenedUrl"]
         return await message.reply(
             f"""Click to copy:\n\n<code>{short_link}</code>.\n\nHere is your [short link]({short_link}).""",
